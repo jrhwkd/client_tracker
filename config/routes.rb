83c178cda074ff503210b77bf2de9f31fe4dac43
Jay <jrhwkd@gmail.com>
2021-06-10 14:31:59 -0500

    create company and group scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :groups
+  
+  resources :companies do
+    resources :groups
+  end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
